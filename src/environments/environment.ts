f255e8675ac1959565e37ceaa542d5bfb7c06bc9
Santiago Nivia <millernivia1301@gmail.com>
2022-12-01 21:11:22 -0500

    Proyecto web solaris

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'solaris-2911',
+    appId: '1:136308801777:web:b8a68138da02d457546950',
+    storageBucket: 'solaris-2911.appspot.com',
+    apiKey: 'AIzaSyC7FhoPb1SXjxJmuyzP3ZTVrOXcvpB3a9E',
+    authDomain: 'solaris-2911.firebaseapp.com',
+    messagingSenderId: '136308801777',
+    measurementId: 'G-RT10FLK79D',
+  },
   production: false
 };
 
